3d2e6b79f039dc44fdcc05b6531bf594472d3f64
frmendez21 <frmendez209@gmail.com>
2021-04-08 14:35:16 -0700

    added padding to errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in?
-     protect_from_forgery unless: -> { request.format.json? }
+        skip_before_action :verify_authenticity_token
+    #  protect_from_forgery unless: -> { request.format.json? }
     def current_user 
         @current_user ||= User.find_by(session_token: session[:session_token])
     end
