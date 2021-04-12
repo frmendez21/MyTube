06d9f287d67437a6c5590f5caa23bfd4c080e495
frmendez21 <frmendez209@gmail.com>
2021-04-11 18:13:55 -0700

    fixed video erorrs

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -11,7 +11,7 @@ class Api::VideosController < ApplicationController
         @video = Video.new(video_params)
         @video.uploader_id = current_user.id
         if @video.save   
-            render :show 
+            render :show  
         else  
             render json: @video.errors.full_messages, status:422
         end 
