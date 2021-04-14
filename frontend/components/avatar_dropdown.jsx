7830db8fbeec55a14aa1596c772bdca133c531ab
frmendez21 <frmendez209@gmail.com>
2021-04-14 15:15:16 -0700

    fixed dropdown

--- frontend/components/avatar_dropdown.jsx
+++ frontend/components/avatar_dropdown.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 export default class AvatarDropdown extends React.Component {
-  
+
     render (){
         const channelLink = window.location.hash !== `#/users/${this.props.currentUser.id}` ?  <Link to={`/users/${this.props.currentUser.id}`}>Your channel</Link> : <a>Your channel</a>
         return (
--- frontend/components/avatar_wrapper.jsx
+++ frontend/components/avatar_wrapper.jsx
@@ -7,14 +7,26 @@ export default class AvatarWrapper extends React.Component {
         this.state = { isDropdownOpen: false}
         this.openDropdown = this.openDropdown.bind(this)
         this.closeDropdown = this.closeDropdown.bind(this)
+        this.handleClickOutside = this.handleClickOutside.bind(this);
     };
 
     openDropdown() {
+       document.addEventListener('mousedown', this.handleClickOutside)
        this.setState({isDropdownOpen: true})
     };
+
     closeDropdown() {
+        document.removeEventListener('mousedown', this.handleClickOutside)
         this.setState({isDropdownOpen: false})
     };
+    
+    handleClickOutside(e) {
+      if ((e.target.className !== 'avatar-list-item') && (e.path[2].className !== 'avatar-dropdown')) {
+          this.closeDropdown();
+      } 
+    }
+
+  
 
     render() {
         const {currentUser, login, logout} = this.props;
@@ -22,7 +34,7 @@ export default class AvatarWrapper extends React.Component {
 
         let button = currentUser ? <button className="avatar-btn" onClick={this.openDropdown}><strong id="avatar-btn-text">{currentUser.username[0].toUpperCase()}</strong></button> : <button className="loggedout-avatar-btn"onClick={login}>SIGN IN</button>;
 
-        let dropdown = isDropdownOpen && currentUser ? <nav className="avatar-dropdown-wrapper" onClick={this.closeDropdown} onMouseLeave={this.closeDropdown}><AvatarDropdown logout={logout} currentUser={currentUser}/></nav> : null;
+        let dropdown = isDropdownOpen && currentUser ? <nav className="avatar-dropdown-wrapper"><AvatarDropdown logout={logout} currentUser={currentUser}/></nav> : null;
         return(
             <div className="avatar-wrapper">
                 {button}
